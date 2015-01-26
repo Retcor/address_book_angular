7c07173a96b804ba9d34910a8cff6b53d1fa7dc6
Dan Baker <bakerz2009@gmail.com>
2015-01-26 11:32:40 -0700

    Added Flyway database migration tool.

--- src/main/java/com/addressbook/model/Address.java
+++ src/main/java/com/addressbook/model/Address.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 @Entity
 public class Address {
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.AUTO)
     private int id;
 
     private String name;
---
+++ src/main/resources/db/migration/V1_2__address_table_schema.sql
@@ -0,0 +1,9 @@
+CREATE TABLE address (
+  id int AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(20),
+  street VARCHAR(20),
+  city VARCHAR(20),
+  state VARCHAR(20),
+  zip VARCHAR(20),
+  phone VARCHAR(20)
+)
